dd9b8ad92cb96c7b76a388c9a0801a08a6082480
Rane Sofia <sofia.luzz.754@gmail.com>
2025-04-05 18:24:29 -0300

    Primeiro commit

---
+++ .vscode/cartassupertrunfo.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(){
+    //meu primeiro trabalho em C
+    int populacao;
+    int pontosturisticos;
+    float area;
+    float pib;
+    char cidade[50];
+    char codigo[10];
+    char estado[3];
+    
+    printf("populacao:\n");
+    scanf("%d",&populacao);
+
+    printf("pontosturisticos:\n");
+    scanf("%s",pontosturisticos);
+
+    printf("area:\n");
+    scanf("%f",&area);
+
+    printf("pib:\n");
+    scanf("%f",&pib);
+
+    printf("cidade:\n");
+    scanf("%s",cidade);
+     
+    printf("codigo:\n");
+    scanf("%s",codigo);
+
+    printf("estado:\n");
+    scanf("%s",estado);
+
+}
---
+++ olamundo.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main () {
+    printf("Hi,i am Rane Sofia!\n");
+    return 0;
+}
